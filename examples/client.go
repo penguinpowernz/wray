30177667a1b5ad167468af7bc1b97200dfcc616e
Robert McLeod <robert@autogrow.com>
2016-02-23 10:23:31 +1300

    commented out the example stuff, it was causing errors

--- examples/client.go
+++ examples/client.go
@@ -1,19 +1,17 @@
 package examples
 
-import (
-	"github.com/autogrowsystems/wray"
-  "fmt"
-)
+// import "github.com/pythonandchips/wray"
+// import "fmt"
 
-func RunClientExample() {
-	wray.RegisterTransports([]wray.Transport{&wray.HttpTransport{}})
-	client := wray.NewFayeClient("http://localhost:5000/faye")
+// func main() {
+// 	wray.RegisterTransports([]wray.Transport{&wray.HttpTransport{}})
+// 	client := wray.NewFayeClient("http://localhost:5000/faye")
 
-	fmt.Println("subscribing")
-	client.Subscribe("/foo", false, func(message wray.Message) {
-		fmt.Println("-------------------------------------------")
-		fmt.Println(message.Data())
-	})
+// 	fmt.Println("subscribing")
+// 	client.Subscribe("/foo", false, func(message wray.Message) {
+// 		fmt.Println("-------------------------------------------")
+// 		fmt.Println(message.Data)
+// 	})
 
-	client.Listen()
-}
+// 	client.Listen()
+// }
--- examples/publish.go
+++ examples/publish.go
@@ -1,17 +1,13 @@
 package examples
 
-import (
-  "github.com/autogrowsystems/wray"
-  "fmt"
-)
+// import "github.com/pythonandchips/wray"
+// import "fmt"
 
-func RunPublisherExample() {
-  wray.RegisterTransports([]wray.Transport{ &wray.HttpTransport{} })
-  client := wray.NewFayeClient("http://localhost:5000/faye")
+// func main() {
+//   wray.RegisterTransports([]wray.Transport{ &gofaye.HttpTransport{} })
+//   client := wray.NewFayeClient("http://localhost:5000/faye")
 
-  params := map[string]interface{}{"hello": "from golang"}
-  fmt.Println("sending")
-  client.Publish("/foo", params)
-}
-
-
+//   params := map[string]interface{}{"hello": "from golang"}
+//   fmt.Println("sending")
+//   client.Publish("/foo", params)
+// }
