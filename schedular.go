c892beb932003bfcb502af5ad27d02ad23fd238c
Robert McLeod <robert@autogrow.com>
2016-05-19 10:43:40 +1200

    unexported ChannelSchedular

--- schedular.go
+++ schedular.go
@@ -2,21 +2,21 @@ package wray
 
 import "time"
 
-type Schedular interface {
+type schedular interface {
 	wait(time.Duration, func())
 	delay() time.Duration
 }
 
-type ChannelSchedular struct {
+type channelSchedular struct {
 }
 
-func (self ChannelSchedular) wait(delay time.Duration, callback func()) {
+func (cs channelSchedular) wait(delay time.Duration, callback func()) {
 	go func() {
 		time.Sleep(delay)
 		callback()
 	}()
 }
 
-func (self ChannelSchedular) delay() time.Duration {
+func (cs channelSchedular) delay() time.Duration {
 	return (1 * time.Minute)
 }
